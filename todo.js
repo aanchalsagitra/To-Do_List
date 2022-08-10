677c1337f5117368f31f64cf60cb94b086aa41a9
aanchal_sagitra <aanchalsagitra260@gmail.com>
2022-08-10 15:10:08 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,45 @@
+const button = document.getElementById("add_item");
+const section = document.getElementById("to_do_list");
+const input = document.getElementById("input");
+
+// ADD ITEM FUNCTION
+const addItem = () => {
+  const para = document.createElement("p");
+  para.innerText = input.value;
+
+  const mainDiv = document.createElement("div");
+  mainDiv.className = "item";
+
+  const iconDiv = document.createElement("div");
+  const checkBox = document.createElement("i");
+  const trashIcon = document.createElement("i");
+
+  checkBox.className = "fas fa-check-square";
+  trashIcon.className = "fas fa-trash";
+
+  checkBox.style.color = "black";
+  trashIcon.style.color = "black";
+  
+  checkBox.addEventListener("click",()=>{
+    checkBox.style.color="black";
+    para.style.textDecoration =
+        "line-through";
+
+
+});
+
+//add  event on trash
+trashIcon.addEventListener("click",()=>{
+mainDiv.remove();
+});
+
+  iconDiv.appendChild(checkBox);
+  iconDiv.appendChild(trashIcon);
+
+  mainDiv.appendChild(para);
+  mainDiv.appendChild(iconDiv);
+  section.appendChild(mainDiv);
+  input.value=""
+  
+};
+button.addEventListener("click", addItem);
